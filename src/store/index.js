20cd35ff059777056e460defc666d967fac7ddfc
Miguel Alonso <malbonm07@gmail.com>
2022-04-09 15:58:10 -0500

    setup vuex

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    podcasts: [],
+    loadingStatus: false,
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
